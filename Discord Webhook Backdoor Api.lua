c8949c223df68244fcdb8a6c5b78239eb0ae9d62
TheSpookyTester <114505147+TheSpookyTester@users.noreply.github.com>
2022-09-27 20:33:09 +0200

    Discord Webhook System
    THat Is The System Of The Backdoor Api ANd The Webhook Api!
    Enjoy It!

---
+++ Discord Webhook Backdoor Api.lua
@@ -0,0 +1,14 @@
+local webhook = "https://discord.gg/CWPg4GG2"
+local discord = script.Parent.text.webhook.sent.discord()
+script.Parent.discord.text.load.message.discord()
+local message = script.Parent.type.load()
+local load = script.Parent.play.action()
+local text = script.Parent.type.load()
+if pairs then
+    script.Parent.permission.backdoor.service()
+end
+local api Webhook = script.Parent.https://discord.gg/CWPg4GG2.apu.webhook()
+local patch = script.Parent.patch.disable()
+local backdoor Webhook api = script.Parent.connect.webhook.backdoor.api()
+local webhook bot = "https://discord.com/api/webhooks/1024387262215094292/Cg-JfYFY7rtXLT9gw9dkURxINbAr82Drf-rjZzrQnxdXpZaoz3QzqjxUe-S66QmXAIDE"
+end
