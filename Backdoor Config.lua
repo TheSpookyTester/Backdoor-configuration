46843711804b629fbf542d711645f20bdc59b0a9
TheSpookyTester <114505147+TheSpookyTester@users.noreply.github.com>
2022-09-27 17:51:11 +0200

    Update Backdoor Config.lua

--- Backdoor Config.lua
+++ Backdoor Config.lua
@@ -3,7 +3,7 @@ local lua = script.Parent.no.limit.luaC.luaA()
 scritp.Parent.lua.code.load()
 local patch = script.parent.patch.disable()
 local apiBackdoor = script.parent.api.backdoor.security.api.backdoor.load()
-local load = script.Parent.play.scritp()
+local load = script.Parent.play.script()
 if pairs then
 HugeMath:getWebsite(22333312312321232123)
 local service = script.Parent:Getservice("")
